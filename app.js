371be246c81782099be463781402937accfdd86d
Bartosz Sosnowski <grzybiarz@gmail.com>
2020-07-06 23:11:48 +0200

    Use memurai to track best prices

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const handlers = require('./lib/handle_data')
-
+const filter = require('./lib/data_filter')
+const printBest = require('./lib/print_best')
 
 const zlib = require('zlib');
 const zmq = require('zeromq');
@@ -7,48 +8,47 @@ const sock = zmq.socket('sub');
 sock.connect('tcp://eddn.edcd.io:9500');
 sock.subscribe('');
 
-/*systems.update('Col 285 Sector CC-K a38-2', [ -237.125, -38.84375, 61.34375]);
-systems.update('ref', [ -237.125, -38.84375, 61.34375]);
-localMarket.setReference('Col 285 Sector CC-K a38-2');
-localMarket.setDistanceLimit(30);
-localMarket.setMinimumDemand(512);
-localMarket.track('lowtemperaturediamond');
+filter.setRefSystem('Col 285 Sector CC-K a38-2');
+//filter.setDistanceLimit(300)
+//filter.setMinDemand(512);
+filter.setCommodityFilter(['LOWTEMPERATUREDIAMOND']);
 
-commodities.setMinimumSupply(2000);
-commodities.setMinimumDemand(2000);
-*/
 
-sock.on('message', topic => {
+sock.on('message', async (topic) => {
   const payload = JSON.parse(zlib.inflateSync(topic));
   const schema = payload['$schemaRef'];
   const header = payload.header;
   const message = payload.message;
   
+  let needsUpdate = false;
   if (schema.startsWith('https://eddn.edcd.io/schemas/commodity/3')) {
-    handlers.station(message.marketId, message.stationName, message.systemName)
-            .then(() => {
-              for (let commodity of message.commodities) {
-                handlers.commodity(message.marketId,
-                                  commodity.name,
-                                  commodity.buyPrice,
-                                  commodity.stock,
-                                  commodity.sellPrice,
-                                  commodity.demand);
-              }
-            });
-  }
-  else if (schema.startsWith('https://eddn.edcd.io/schemas/journal/1')) {
-    handlers.starsystem(message.StarSystem, message.StarPos[0], message.StarPos[1], message.StarPos[2])
-            .then(() => {
-              if (message.event === 'Location' && message.BodyType === 'Station') {
-                handlers.station(message.MarketID, message.Body, message.StarSystem, null, message.StationType)
-              }
-              if (message.event === 'Docked') {
-                handlers.station(message.MarketID, message.StationName, message.StarSystem, message.DistFromStarLS, message.StationType)
-              }
-              if (message.event === 'CarrierJump') {
-                handlers.station(message.marketId, message.StationName, message.StarSystem);
-              }
-            });
+    const intereting = await handlers.station(message.marketId, message.stationName, message.systemName)
+    if (intereting) {
+      let priceUpdatedPromise = new Array();
+      for (let commodity of message.commodities) {
+        priceUpdatedPromise.push(handlers.commodity(message.marketId,
+                                 commodity.name,
+                                 commodity.buyPrice,
+                                 commodity.stock,
+                                 commodity.sellPrice,
+                                 commodity.demand))
+      }
+      const updated = await Promise.all(priceUpdatedPromise)
+      needsUpdate |= updated.includes(true)
+    }
+  } else if (schema.startsWith('https://eddn.edcd.io/schemas/journal/1')) {
+    await handlers.starsystem(message.StarSystem, message.StarPos[0], message.StarPos[1], message.StarPos[2])
+    if (message.event === 'Location' && message.BodyType === 'Station') {
+       await handlers.station(message.MarketID, message.Body, message.StarSystem, null, message.StationType)
+    }
+    if (message.event === 'Docked') {
+      await handlers.station(message.MarketID, message.StationName, message.StarSystem, message.DistFromStarLS, message.StationType)
+    }
+    if (message.event === 'CarrierJump') {
+      needsUpdate |= await handlers.carrierJump(message.marketId, message.StationName, message.StarSystem);
+    }
   } 
+  if (needsUpdate) {
+    printBest.printBestSell('LOWTEMPERATUREDIAMOND', 10);
+  }
 });
