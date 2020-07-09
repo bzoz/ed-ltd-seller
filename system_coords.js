d556bc09d1c8c6ba217ea6d5b01878922f77f2e0
Bartosz Sosnowski <bartosz@janeasystems.com>
2020-07-09 14:27:17 +0200

    Make getting coords work better with multiple instances of the tracker running

--- system_coords.js
+++ system_coords.js
@@ -1,18 +1,32 @@
 const level = require('level')
-const db = level('systems-leveldb')
 
-if (process.argv.length < 3) {
-  console.log('Usage: node system_coords.js system name')
-} else {
-  (async () => {
-    const name = process.argv.slice(2).join(' ')
-    try {
-      const coordsTxt = await db.get(name.toUpperCase());
-      const coords = coordsTxt.split(',');
-      console.log(`${coords[0]},${coords[1]},${coords[2]}`)
-    } catch {
-      console.log(`Unkonw system: ${name}`)
-      process.exit(-1)
+function get_coords() {
+  level('systems-leveldb', (err, db)=> {
+    if (err) {
+      if (err instanceof level.errors.OpenError) {
+        // try agian in 50ms
+        setTimeout(get_coords, 50)
+        return
+      } else {
+        throw err
+      }
     }
-  })();
+    if (process.argv.length < 3) {
+      console.log('Usage: node system_coords.js system name')
+    } else {
+      (async () => {
+        const name = process.argv.slice(2).join(' ')
+        try {
+          const coordsTxt = await db.get(name.toUpperCase());
+          const coords = coordsTxt.split(',');
+          console.log(`${coords[0]},${coords[1]},${coords[2]}`)
+        } catch {
+          console.log(`Unkonw system: ${name}`)
+          process.exit(-1)
+        }
+      })();
+    }
+  })
 }
+
+get_coords()
